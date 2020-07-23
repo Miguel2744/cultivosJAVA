837adc8a39c222db5754d473a8d60a9742fb9578
DESKTOP-EG33FMS\ASUS <L18090234@zacatepec.tecnm.mx>
2020-07-23 00:10:51 -0500

    terminado el diseño

---
+++ src/proyectocultivos/ProyectoCultivos.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectocultivos;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+
+/*
+public class ProyectoCultivos {
+    
+    
+    private static Principal formulario;
+    private static persona Persona[ ];
+    private static int contador;
+
+    /**
+     * @param args the command line arguments
+     */
+/*
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        formulario= new Principal ();
+        Persona=new persona[100];
+        // creamos un arreglo  para agregar 0 - 99 elementos
+        // del arreglo 
+        
+        for (int i = 0; i < 100;  i++){
+            Persona[i]=new persona( ) ;
+        }
+        contador =0;
+        
+        formulario.setVisible (true);
+        //creamos metodos guardar y buscar 
+    }
+    
+    public static void guardar (String n, String a, String c){
+        // creamos 4 variables 
+        
+        Persona[contador].setLonZona(n);
+        Persona[contador].setZonanombre(a);
+         Persona[contador].setLatitudT(c);
+         contador++;
+         
+    }
+    
+    public static void buscar (String bus) {
+        
+        for (int j=0; j<Persona.length; j++){
+            if(Persona[j].getLatitudT().equals(bus)){
+                formulario.cargardatos(Persona[j]);
+            }
+        }
+        
+        
+        
+    }
+        
+        
+        
+
+       
+     
+        
+      
+    
+    
+}
+    
+    */   
