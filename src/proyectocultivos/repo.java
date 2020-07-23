c4e9af1fc4c90285aad51c013712c9dc6c4ede28
DESKTOP-EG33FMS\ASUS <L18090234@zacatepec.tecnm.mx>
2020-07-23 00:19:58 -0500

    ejemplo para subir al repo

---
+++ src/proyectocultivos/repo.java
@@ -0,0 +1,31 @@
+package proyectocultivos;
+
+import javax.swing.JOptionPane;
+
+public class repo{
+    
+    public static void main (String [] args){
+        
+         JOptionPane. showMessageDialog(null,  "Liz No Tiene Novio xD (Me avisas si sirve )" );
+        
+        /*  ejemplo 2 de aporte probando*/
+        
+        JOptionPane. showMessageDialog(null,  "aldrich tu turno" );
+        
+             /*  ejemplo 3 de aporte probando*/
+        
+        JOptionPane. showMessageDialog(null,  "Liz tu turno" );
+        
+        
+          JOptionPane. showMessageDialog(null,  "ya quedo, esto solo es prueba " );
+          
+        
+         JOptionPane. showMessageDialog(null,  "ya quedo, esto solo es 2da prueba 1:44 am zzzzz " );
+         JOptionPane. showMessageDialog(null,  "ya quedo, esto solo es 2da prueba 1:44 am zzzzz " );
+         JOptionPane. showMessageDialog(null,  "ya quedo, esto solo es 2da prueba 1:44 am zzzzz " );
+         JOptionPane. showMessageDialog(null,  "ya quedo, esto solo es 2da prueba 1:44 am zzzzz " );
+        
+        
+    }
+    
+}
